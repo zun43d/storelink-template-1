2b862aae94fd25f1bd855a58172fc97b1aa90299
Zunaed Ahmed <65343827+zun43d@users.noreply.github.com>
2025-05-27 05:03:57 +0000

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -2,14 +2,6 @@ import { PrismaClient } from '@prisma/client'
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    datasources: {
-      db: {
-        url: process.env.DATABASE_URL,
-      },
-    },
-  })
+export const prisma = globalForPrisma.prisma || new PrismaClient()
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
