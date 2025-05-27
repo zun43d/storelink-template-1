4ece3bcb599d6041b5fbece234bd4863f7c18d14
Zunaed Ahmed <65343827+zun43d@users.noreply.github.com>
2025-05-27 05:27:39 +0000

    update

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,14 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient }
+let prisma: PrismaClient;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+export default prisma;
